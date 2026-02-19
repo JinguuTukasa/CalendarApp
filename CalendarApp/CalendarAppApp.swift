ba66382a2658e33ab664f3577ebb43cd07e3d6b5
JinguuTukasa <desusousu0@gmail.com>
2026-02-20 00:09:44 +0900

    Initial project setup

---
+++ CalendarApp/CalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppApp.swift
+//  CalendarApp
+//
+//  Created by 藤井竣平 on 2026/02/20.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarAppTests/CalendarAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppTests.swift
+//  CalendarAppTests
+//
+//  Created by 藤井竣平 on 2026/02/20.
+//
+
+import Testing
+@testable import CalendarApp
+
+struct CalendarAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
